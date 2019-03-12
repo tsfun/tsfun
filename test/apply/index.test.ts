3caddde245fdfadab556b03452c2adbdc2f7cc12
khai96_ <hvksmr1996@gmail.com>
2019-03-12 21:59:09 +0700

    Rename @tsfun/args to @tsfun/apply

--- test/apply/index.test.ts
+++ test/apply/index.test.ts
@@ -1,4 +1,4 @@
-import { apply, unapply } from '@tsfun/args'
+import { apply, unapply } from '@tsfun/apply'
 
 it('apply works as expected', () => {
   const join = (...args: number[]) => args.join(', ')
