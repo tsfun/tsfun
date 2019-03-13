74bab278ab5aaea18588dd8ddc2b78da7d800b31
khai96_ <hvksmr1996@gmail.com>
2019-03-13 18:04:08 +0700

    Set default generic parameter for map

--- packages/option/map.ts
+++ packages/option/map.ts
@@ -7,7 +7,7 @@ import { some } from './some'
  * @param fn Function to call when `option` contains a value
  * @returns An `Option` of `fn`'s return value
  */
-export const map = <Value, Return = Value> (
+export const map = <Value = never, Return = Value> (
   option: Option<Value>,
   fn: (value: Value) => Return
 ) => option.tag ? some(fn(option.value)) : option
