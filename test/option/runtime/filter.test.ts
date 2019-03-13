482dba71e304d4d4d9e6c0533e3b9626fe2da03e
khai96_ <hvksmr1996@gmail.com>
2019-03-13 12:48:13 +0700

    Add test for filter

---
+++ test/option/runtime/filter.test.ts
@@ -0,0 +1,21 @@
+import { filter, some, none } from '@tsfun/option'
+
+it('works with Some', () => {
+  const isEven = (x: number) => x % 2 === 0
+  expect(
+    [0, 1, 2, 3, 4, 5]
+      .map(some)
+      .map(x => filter(x, isEven))
+  ).toEqual([
+    some(0),
+    none(),
+    some(2),
+    none(),
+    some(4),
+    none()
+  ])
+})
+
+it('works with None', () => {
+  expect(filter(none(), () => true)).toEqual(none())
+})
