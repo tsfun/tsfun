233d758ea7733114e0d6b386cc187e30ef8bd142
khai96_ <hvksmr1996@gmail.com>
2019-03-14 19:47:17 +0700

    Type test for option

---
+++ test/option/typecheck/option.ts
@@ -0,0 +1,13 @@
+import assert from 'static-type-assert'
+import { Option, Some, None, option } from '@tsfun/option'
+
+assert<Some<'x'>>(option('x' as 'x'))
+assert<Some<0>>(option(0 as 0))
+assert<Some<false>>(option(false as false))
+
+assert<None>(option(undefined))
+assert<None>(option(null))
+
+assert<Option<any>>(option(0 as any))
+assert<Option<0>>(option(0 as 0 | undefined))
+assert<Option<false>>(option(false as false | null))
