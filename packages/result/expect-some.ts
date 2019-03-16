dd79c734e9728cf6ea413c1f959d6eab82b14830
khai96_ <hvksmr1996@gmail.com>
2019-03-16 12:51:38 +0700

    Create expect-some.ts

---
+++ packages/result/expect-some.ts
@@ -0,0 +1,37 @@
+import { Option } from '@tsfun/option'
+import { Result } from './result'
+import { err } from './err'
+
+/**
+ * If `option` is `some(x)`, return `ok(x)`,
+ * otherwise return an `Err`
+ * @param option Option to match against
+ * @returns `Result`
+ */
+export const expectSome =
+  <Payload = never> (option: Option<Payload>): Result<Payload, Error> =>
+    expectSomeOrElse(option, () => new Error('Expecting Some but received None'))
+
+/**
+ * If `option` is `some(x)`, return `ok(x)`,
+ * otherwise return `err(error)`
+ * @param option Option to match against
+ * @param error Error to carry should `option` is a `None`
+ * @returns `Result`
+ */
+export const expectSomeOr = <Payload = never, Error = never> (
+  option: Option<Payload>,
+  error: Error
+): Result<Payload, Error> => option.tag ? option : err(error)
+
+/**
+ * If `option` is `some(x)`, return `ok(x)`,
+ * otherwise return `err(error())`
+ * @param option Option to match against
+ * @param error Function to execute should `option` is a `None`
+ * @returns `Result`
+ */
+export const expectSomeOrElse = <Payload = never, Error = never> (
+  option: Option<Payload>,
+  error: () => Error
+): Result<Payload, Error> => option.tag ? option : err(error())
--- packages/result/index.ts
+++ packages/result/index.ts
@@ -1,3 +1,4 @@
 export * from './result'
 export * from './ok'
 export * from './err'
+export * from './expect-some'
