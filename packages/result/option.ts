daf20e8200e8e03484b02307016aa8ddce477d83
khai96_ <hvksmr1996@gmail.com>
2019-03-17 17:26:21 +0700

    Create function that "converts" a result to an option

---
+++ packages/result/option.ts
@@ -0,0 +1,15 @@
+import { Option } from '@tsfun/option'
+import { Result } from './result'
+
+/**
+ * Convert `ok(x)` to an `some(x)`
+ * or `err(_)` to `none()`
+ * @param result Result to convert
+ * @returns Corresponding `Option`
+ *
+ * In reality, this function returns the same object it takes
+ * since `Result` interface is compatible with `Option`
+ */
+export const option = <Payload = never> (result: Result<Payload, any>): Option<Payload> => result
+
+export default option
