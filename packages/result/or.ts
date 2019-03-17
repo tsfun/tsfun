71f48e33fc8a87c93fb24a22410dcdae59346e01
khai96_ <hvksmr1996@gmail.com>
2019-03-17 23:04:04 +0700

    Create @tsfun/result/or

---
+++ packages/result/or.ts
@@ -0,0 +1,33 @@
+import { Result } from './result'
+
+/**
+ * Return `left` if it carries a payload,
+ * otherwise return `right`
+ * @param left Result to match against
+ * @param right Result to return when `left` carries an error
+ * @returns Either `left` or `right`
+ */
+export const or = <
+  Payload = never,
+  Left = never,
+  Right = Left
+> (
+  left: Result<Payload, Left>,
+  right: Result<Payload, Right>
+) => left.tag ? left : right
+
+/**
+ * Return `left` if it carries a payload,
+ * otherwise execute `right` with error of `left` and return the result
+ * @param left Result to match against
+ * @param right Function to call when `left` carries an error
+ * @returns Either `left` or return value of `right`
+ */
+export const orElse = <
+  Payload = never,
+  Left = never,
+  Right = Left
+> (
+  left: Result<Payload, Left>,
+  right: (error: Left) => Result<Payload, Right>
+) => left.tag ? left : right(left.error)
