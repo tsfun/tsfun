60e93f2cf7576a0945a19d8a34bf7616a019b041
khai96_ <hvksmr1996@gmail.com>
2019-03-18 14:00:49 +0700

    Create @tsfun/result/unwrap

---
+++ packages/result/unwrap.ts
@@ -0,0 +1,45 @@
+import { Result } from './result'
+import { match } from './match'
+
+/**
+ * Return `x` if `result` is `ok(x)`,
+ * and throw `error` if `result` is `err(error)`
+ * @param result Result to unwrap
+ * @returns Carried payload
+ */
+export const unwrap = <
+  Payload = never,
+  Error = never
+> (result: Result<Payload, Error>) => unwrapOrElse(result, error => { throw error })
+
+/**
+ * Return `x` if `result` is `ok(x)`,
+ * otherwise return `def`
+ * @param result Result to unwrap
+ * @param def Value to return when `result` carries an error
+ * @returns Either carried payload or `def`
+ */
+export const unwrapOr = <
+  Payload = never,
+  Default = Payload,
+  Error = never
+> (
+  result: Result<Payload, Error>,
+  def: Default
+) => unwrapOrElse(result, () => def)
+
+/**
+ * Return `x` if `result` is `ok(x)`,
+ * and return `def(error)` if `result` is `err(error)`
+ * @param result Result to unwrap
+ * @param def Function to call when `result` carries an error
+ * @returns Either carried payload of `result` or returning value of `def`
+ */
+export const unwrapOrElse = <
+  Payload = never,
+  Default = Payload,
+  Error = never
+> (
+  result: Result<Payload, Error>,
+  def: (error: Error) => Default
+) => match(result, { ok: x => x, err: def })
