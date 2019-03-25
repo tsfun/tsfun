120936043aa16f402768af55a0aa739a44d80c03
khai96_ <hvksmr1996@gmail.com>
2019-03-25 14:46:17 +0700

    Create clone.test.ts

---
+++ test/result/runtime/clone.test.ts
@@ -0,0 +1,27 @@
+import { Result, clone, ok, err } from '@tsfun/result'
+
+const init = <X = never, E = never> (original: Result<X, E>) => ({ original, copy: clone(original) })
+
+describe('clone(ok(x))', () => {
+  const { original, copy } = init(ok('payload'))
+
+  it('original and copy are not the same object', () => {
+    expect(copy).not.toBe(original)
+  })
+
+  it('original and copy have identical content', () => {
+    expect(copy).toEqual(original)
+  })
+})
+
+describe('clone(err(e))', () => {
+  const { original, copy } = init(err('error'))
+
+  it('original and copy are not the same object', () => {
+    expect(copy).not.toBe(original)
+  })
+
+  it('original and copy have identical content', () => {
+    expect(copy).toEqual(original)
+  })
+})
