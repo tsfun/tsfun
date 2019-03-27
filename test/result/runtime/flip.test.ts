69f3730a135d7d5b5fb97063605f55dd435e9eaa
khai96_ <hvksmr1996@gmail.com>
2019-03-27 16:15:20 +0700

    Grammar

--- test/result/runtime/flip.test.ts
+++ test/result/runtime/flip.test.ts
@@ -1,9 +1,9 @@
 import { flip, ok, err } from '@tsfun/result'
 
-it('flip an ok into an err', () => {
+it('flips an ok into an err', () => {
   expect(flip(ok('x'))).toEqual(err('x'))
 })
 
-it('flip an err into an ok', () => {
+it('flips an err into an ok', () => {
   expect(flip(err('x'))).toEqual(ok('x'))
 })
