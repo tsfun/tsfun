a895dd8bda5fdd74fc20479c780345a2b951e0b9
khai96_ <hvksmr1996@gmail.com>
2019-03-29 22:02:58 +0700

    Create flatten.test.ts

---
+++ test/result/runtime/flatten.test.ts
@@ -0,0 +1,29 @@
+import { flatten, flattenError, ok, err } from '@tsfun/result'
+
+describe('flatten', () => {
+  it('works with Ok<Ok<X>>', () => {
+    expect(flatten(ok(ok('x')))).toEqual(ok('x'))
+  })
+
+  it('works with Ok<Err<X>>', () => {
+    expect(flatten(ok(err('x')))).toEqual(err('x'))
+  })
+
+  it('works with Err<X>', () => {
+    expect(flatten(err('x'))).toEqual(err('x'))
+  })
+})
+
+describe('flattenError', () => {
+  it('works with Err<Err<X>>', () => {
+    expect(flattenError(err(err('x')))).toEqual(err('x'))
+  })
+
+  it('works with Err<Ok<X>>', () => {
+    expect(flattenError(err(ok('x')))).toEqual(ok('x'))
+  })
+
+  it('works with Ok<X>', () => {
+    expect(flattenError(ok('x'))).toEqual(ok('x'))
+  })
+})
