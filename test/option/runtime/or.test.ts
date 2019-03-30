abe1ba358735a5d0b7ccc2bcdba86a3438b0c071
khai96_ <hvksmr1996@gmail.com>
2019-03-30 13:35:23 +0700

    Properly test callback functions in or.test.ts

--- test/option/runtime/or.test.ts
+++ test/option/runtime/or.test.ts
@@ -24,19 +24,66 @@ describe('and', () => {
 })
 
 describe('orElse', () => {
-  it('between some and some', () => {
-    expect(orElse(some('a'), () => some('b'))).toEqual(some('a'))
+  const mkSomeFn = () => jest.fn(() => some('b'))
+  const mkNoneFn = () => jest.fn(none)
+
+  describe('between some and some', () => {
+    const right = mkSomeFn()
+    const result = orElse(some('a'), right)
+
+    it('returns expected value', () => {
+      expect(result).toEqual(some('a'))
+    })
+
+    it('does not call right', () => {
+      expect(right).not.toBeCalled()
+    })
   })
 
-  it('between some and none', () => {
-    expect(orElse(some('a'), none)).toEqual(some('a'))
+  describe('between some and none', () => {
+    const right = mkNoneFn()
+    const result = orElse(some('a'), right)
+
+    it('returns expected value', () => {
+      expect(result).toEqual(some('a'))
+    })
+
+    it('does not call right', () => {
+      expect(right).not.toBeCalled()
+    })
   })
 
-  it('between none and some', () => {
-    expect(orElse(none(), () => some('b'))).toEqual(some('b'))
+  describe('between none and some', () => {
+    const right = mkSomeFn()
+    const result = orElse(none(), right)
+
+    it('returns expected value', () => {
+      expect(result).toEqual(some('b'))
+    })
+
+    it('calls right exactly once', () => {
+      expect(right).toBeCalledTimes(1)
+    })
+
+    it('calls right no arguments', () => {
+      expect(right).toBeCalledWith()
+    })
   })
 
-  it('between none and none', () => {
-    expect(orElse(none(), none)).toEqual(none())
+  describe('between none and none', () => {
+    const right = mkNoneFn()
+    const result = orElse(none(), right)
+
+    it('returns expected value', () => {
+      expect(result).toEqual(none())
+    })
+
+    it('calls right exactly once', () => {
+      expect(right).toBeCalledTimes(1)
+    })
+
+    it('calls right with no arguments', () => {
+      expect(right).toBeCalledWith()
+    })
   })
 })
