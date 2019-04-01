c0186d6c49ef14cad189cf40a3ea2ef1d55e3e7d
khai96_ <hvksmr1996@gmail.com>
2019-04-01 13:16:45 +0700

    Create match.test.ts

---
+++ test/result/runtime/match.test.ts
@@ -0,0 +1,48 @@
+import { match, ok, err } from '@tsfun/result'
+
+const fns = () => ({
+  ok: jest.fn((x: string) => `Ok "${x}"`),
+  err: jest.fn((x: string) => `Err "${x}"`)
+})
+
+describe('with Ok', () => {
+  const handlers = fns()
+  const result = match(ok('x'), handlers)
+
+  it('returns expected result', () => {
+    expect(result).toEqual('Ok "x"')
+  })
+
+  it('calls handlers.ok exactly once', () => {
+    expect(handlers.ok).toBeCalledTimes(1)
+  })
+
+  it('calls handlers.ok with carried payload', () => {
+    expect(handlers.ok).toBeCalledWith('x')
+  })
+
+  it('does not call handlers.err', () => {
+    expect(handlers.err).not.toBeCalled()
+  })
+})
+
+describe('with Err', () => {
+  const handlers = fns()
+  const result = match(err('x'), handlers)
+
+  it('returns expected result', () => {
+    expect(result).toEqual('Err "x"')
+  })
+
+  it('calls handlers.err exactly once', () => {
+    expect(handlers.err).toBeCalledTimes(1)
+  })
+
+  it('calls handlers.err with carried payload', () => {
+    expect(handlers.err).toBeCalledWith('x')
+  })
+
+  it('does not call handlers.ok', () => {
+    expect(handlers.ok).not.toBeCalled()
+  })
+})
