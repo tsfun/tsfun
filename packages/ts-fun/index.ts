3aa18228f3b9b77e97696f5cd33861d47686f283
khai96_ <hvksmr1996@gmail.com>
2019-04-03 14:04:30 +0700

    Create ts-fun

---
+++ packages/ts-fun/index.ts
@@ -0,0 +1,11 @@
+import * as apply from '@ts-fun/apply'
+export { apply }
+
+import * as pipe from '@ts-fun/pipe'
+export { pipe }
+
+import * as option from '@ts-fun/option'
+export { option }
+
+import * as result from '@ts-fun/result'
+export { result }
