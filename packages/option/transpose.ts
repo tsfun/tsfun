0ce17d9071718d355e360d59d64d50d43fa2acd1
khai96_ <hvksmr1996@gmail.com>
2019-04-04 23:06:10 +0700

    Create transpose

---
+++ packages/option/transpose.ts
@@ -0,0 +1,18 @@
+import { Result, ok } from '@tsfun/prv-option-result-common'
+import { Option } from './option'
+
+/**
+ * Transposes an `Option` of `Result` into a `Result` of `Option`
+ * @param option `Option` of `Result`
+ * @returns `Result` of `Option`
+ */
+export function transpose <
+  Payload = never,
+  Error = never
+> (option: Option<Result<Payload, Error>>): Result<Option<Payload>, Error> {
+  if (!option.tag) return ok(option)
+  const result = option.value
+  return result.tag ? ok(result) : result
+}
+
+export default transpose
---
+++ packages/result/transpose.ts
@@ -0,0 +1,18 @@
+import { Option, some } from '@tsfun/prv-option-result-common'
+import { Result } from './result'
+
+/**
+ * Transpose a `Result` of `Option` into an `Option` of `Result`
+ * @param result `Result` of `Option`
+ * @returns `Option` of `Result`
+ */
+export function transpose <
+  Payload = never,
+  Error = never
+> (result: Result<Option<Payload>, Error>): Option<Result<Payload, Error>> {
+  if (!result.tag) return some(result)
+  const option = result.value
+  return option.tag ? some(option) : option
+}
+
+export default transpose
