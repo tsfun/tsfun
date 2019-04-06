60b7958cf5e06c54326e5c5a711aef8c45e96982
khai96_ <hvksmr1996@gmail.com>
2019-04-06 16:08:56 +0700

    Create @tsfun/result/try-exec

---
+++ packages/result/try-exec.ts
@@ -0,0 +1,23 @@
+import { Result } from './result'
+import { ok } from './ok'
+import { err } from './err'
+
+/**
+ * Return `ok(x)` if executing `fn` returns `x`,
+ * and return `err(error)` if executing `fn` throws `error`
+ * @param fn Function to execute
+ * @param args Arguments to pass to function
+ * @returns `Result` of function execution
+ */
+export function tryExec <Args extends any[], Return> (
+  fn: (...args: Args) => Return,
+  ...args: Args
+): Result<Return, any> {
+  try {
+    return ok(fn(...args))
+  } catch (error) {
+    return err(error)
+  }
+}
+
+export default tryExec
