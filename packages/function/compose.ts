ff38938455b8d59f18e8c601a28efedbbc348888
khai96_ <hvksmr1996@gmail.com>
2019-09-23 15:12:35 +0700

    Add composeXs[Right]

--- packages/function/compose.ts
+++ packages/function/compose.ts
@@ -11,6 +11,17 @@ export const compose = <X, Y, Z> (
 
 /**
  * Compose two function
+ * @param f Function that takes `y` and returns `z`
+ * @param g Function that takes `...xs` and returns `y`
+ * @returns Function that takes `...xs` and returns `z = f(g(...xs))`
+ */
+export const composeXs = <Xs extends any[], Y, Z> (
+  f: (y: Y) => Z,
+  g: (...xs: Xs) => Y
+) => (...xs: Xs) => f(g(...xs))
+
+/**
+ * Compose two function
  * @param f Function that takes `x` and returns `y`
  * @param g Function that takes `y` and returns `x`
  * @returns Function that takes `x` and returns `z = g(f(x))`
@@ -19,6 +30,17 @@ export const composeRight = <X, Y, Z> (
   f: (x: X) => Y,
   g: (y: Y) => Z
 ) => compose(g, f)
+
+/**
+ * Compose two function
+ * @param f Function that takes `...xs` and returns `y`
+ * @param g Function that takes `y` and returns `z`
+ * @returns Function that takes `...xs` and returns `z = g(f(...xs))`
+ */
+export const composeXsRight = <Xs extends any[], Y, Z> (
+  f: (...xs: Xs) => Y,
+  g: (y: Y) => Z
+) => composeXs(g, f)
 
 export {
   compose as composeFns,
