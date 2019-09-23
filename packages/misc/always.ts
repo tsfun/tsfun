0f0d59da2b831f262ffee5f1204c56e609ae0a40
khai96_ <hvksmr1996@gmail.com>
2019-09-23 13:26:28 +0700

    Make an alias to always called 'nullary'

--- packages/misc/always.ts
+++ packages/misc/always.ts
@@ -1,7 +1,8 @@
 /**
- * Turn a value into a function that returns the value
+ * Turn a value into a nullary function that returns the value
  * @param x Value in box
  * @returns A function that takes no arguments and returns `value`
  */
 export const always = <X> (x: X) => () => x
+export { always as nullary }
 export default always
