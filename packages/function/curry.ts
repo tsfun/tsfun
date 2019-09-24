18046436643ca551c7fc301094a68efb70de616a
khai96_ <hvksmr1996@gmail.com>
2019-09-24 11:10:58 +0700

    Create uncurry

--- packages/function/curry.ts
+++ packages/function/curry.ts
@@ -1,5 +1,7 @@
 export type Curriable<X, Xs extends any[], Y> = (x: X, ...xs: Xs) => Y
 export type Curried<X, Xs extends any[], Y> = (...xs: Xs) => (x: X) => Y
+export type Uncurriable<X, Xs extends any[], Y> = (...xs: Xs) => (x: X) => Y
+export type Uncurried<X, Xs extends any[], Y> = (x: X, ...xs: Xs) => Y
 
 /**
  * Postpone the first argument of a function
@@ -11,4 +13,12 @@ export const curry =
     (fn: Curriable<X, Xs, Y>): Curried<X, Xs, Y> =>
       (...xs) => x => fn(x, ...xs)
 
-export default curry
+/**
+ * Uncurry a function that is result of `curry`
+ * @param fn Function to uncurry
+ * @returns Uncurried function
+ */
+export const uncurry =
+  <X, Xs extends any[], Y>
+    (fn: Uncurriable<X, Xs, Y>): Uncurried<X, Xs, Y> =>
+      (x, ...xs) => fn(...xs)(x)
