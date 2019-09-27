49154c07b5df6a302bc2e119e8cc94cdd5b042b6
khai96_ <hvksmr1996@gmail.com>
2019-09-27 12:31:25 +0700

    Ignore names start with 'prv-'

--- tools/docs/lib/config.ts
+++ tools/docs/lib/config.ts
@@ -6,4 +6,6 @@ export const title = 'API Reference'
 /**
  * Glob patterns that match package names to be ignored
  */
-export const ignoredPackages = [] as const
+export const ignoredPackages = [
+  'prv-*'
+] as const
