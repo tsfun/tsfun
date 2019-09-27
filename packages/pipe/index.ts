1945c0e0df0b0e7152d5869e16d569e645b65b2c
khai96_ <hvksmr1996@gmail.com>
2019-09-27 13:09:05 +0700

    Change jsdoc to a format that can be read by TypeDoc

--- packages/pipe/index.ts
+++ packages/pipe/index.ts
@@ -35,89 +35,105 @@ class PipeNode<Args extends any[], Return> {
 /**
  * Start a pipeline with a value
  * (pass a value to a series of functions in a pipeline)
+ *
+ * **Example:**
+ *
+ * ```typescript
+ * const increase = (x: number) => x + 1
+ * const double = (x: number) => x * 2
+ * const square = (x: number) => x * x
+ * const result = pass(2) // x0 = 2
+ *   .to(increase) // x1 = increase(x0)
+ *   .to(double) // x2 = double(x1)
+ *   .to(square) // x3 = square(x2)
+ *   .to(String) // x4 = String(x3)
+ *   .get() // result = x4 = String(square(double(increase(2))))
+ * expect(result).toBe(String(square(double(increase(2)))))
+ * ```
+ *
+ * **Example:**
+ *
+ * ```typescript
+ * const sum = (...args: number[]) => args.reduce((a, b) => a + b)
+ * const product = (...args: number[]) => args.reduce((a, b) => a * b)
+ * const result = pass(2) // x0 = 2
+ *   .to(sum, -3, 4) // x1 = sum(x0, -3, 4)
+ *   .to(product, 2) // x2 = product(x1, 2)
+ *   .to(String) // x3 = String(x2)
+ *   .get() // result = x3 = String(product(sum(2, -3, 4), 2))
+ * expect(result).toBe(String(product(sum(2, -3, 4), 2)))
+ * ```
+ *
+ * **Usage:**
+ *
+ * `pass(x).to(f).to(g).to(h).get()` is equivalent to `x |> f |> g |> h`,
+ * which in turn is equivalent to `h(g(f(x)))`
+ *
+ * You can also pass extra arguments to function that takes more than one argument,
+ * for example: `pass(x).to(f, y, z).get()` is equivalent to `f(x, y, z)`
+ *
+ * **Note:**
+ *
+ * This is a temporary solution to replace yet-to-be-implemented pipeline operator.
+ * This function will be deprecated when the proposal get to stage 3
+ * and removed when it is implemented
+ *
+ * Read more about pipeline operator in
+ * [the tc39 proposal](https://github.com/tc39/proposal-pipeline-operator)
+ *
  * @param x First value in the pipeline
  * @returns First node of the pipeline
- *
- * @usage
- *   `pass(x).to(f).to(g).to(h).get()` is equivalent to `x |> f |> g |> h`,
- *   which in turn is equivalent to `h(g(f(x)))`
- *
- *   You can also pass extra arguments to function that takes more than one argument,
- *   for example: `pass(x).to(f, y, z).get()` is equivalent to `f(x, y, z)`
- *
- * @note
- *   This is a temporary solution to replace yet-to-be-implemented pipeline operator.
- *   This function will be deprecated when the proposal get to stage 3
- *   and removed when it is implemented
- *
- *   Read more about pipeline operator in
- *   [the tc39 proposal](https://github.com/tc39/proposal-pipeline-operator)
- *
- * @example
- *   const increase = (x: number) => x + 1
- *   const double = (x: number) => x * 2
- *   const square = (x: number) => x * x
- *   const result = pass(2) // x0 = 2
- *     .to(increase) // x1 = increase(x0)
- *     .to(double) // x2 = double(x1)
- *     .to(square) // x3 = square(x2)
- *     .to(String) // x4 = String(x3)
- *     .get() // result = x4 = String(square(double(increase(2))))
- *   expect(result).toBe(String(square(double(increase(2)))))
- *
- * @example
- *   const sum = (...args: number[]) => args.reduce((a, b) => a + b)
- *   const product = (...args: number[]) => args.reduce((a, b) => a * b)
- *   const result = pass(2) // x0 = 2
- *     .to(sum, -3, 4) // x1 = sum(x0, -3, 4)
- *     .to(product, 2) // x2 = product(x1, 2)
- *     .to(String) // x3 = String(x2)
- *     .get() // result = x3 = String(product(sum(2, -3, 4), 2))
- *   expect(result).toBe(String(product(sum(2, -3, 4), 2)))
  */
 export const pass = <X> (x: X) => pipe(() => x)
 
 /**
  * Start a pipeline with a function
+ *
+ * **Example:**
+ *
+ * ```typescript
+ * const sum = (...args: number[]) => args.reduce((a, b) => a + b, 0)
+ * const increase = (x: number) => x + 1
+ * const double = (x: number) => x * 2
+ * const square = (x: number) => x * x
+ * const pipeline = pipe(sum) // x0 = sum(...args)
+ *   .to(increase) // x1 = increase(x0)
+ *   .to(double) // x2 = double(x1)
+ *   .to(square) // x3 = square(x2)
+ *   .to(String) // x4 = String(x3)
+ *   .get // pipeline(...args) = x4 = String(square(double(increase(sum(...args)))))
+ * expect(pipeline(2, 3, -1)).toBe(String(square(double(increase(sum(2, 3, -1))))))
+ * ```
+ *
+ * **Example:**
+ *
+ * ```typescript
+ * const sum = (...args: number[]) => args.reduce((a, b) => a + b)
+ * const product = (...args: number[]) => args.reduce((a, b) => a * b)
+ * const pipeline = pipe(sum) // x0 = sum(...args)
+ *   .to(product, 2) // x1 = product(x0, 2)
+ *   .to(String) // x2 = String(x1)
+ *   .get // pipeline(...args) = x2 = String(product(sum(2, -3, 4), 2))
+ * expect(pipeline(2, -3, 4)).toBe(String(product(sum(2, -3, 4), 2)))
+ * ```
+ *
+ * **Usage:**
+ * `pipe(f).to(g).to(h).get(...args)` is equivalent to `f(...args) |> g |> h`,
+ * which in turn is equivalent to `h(g(f(...args)))`
+ *
+ * You can also pass extra arguments to function that takes more than takes more than one arguments,
+ * for example: `pipe(f).to(g, a, b).get` is equivalent to `(...args) => g(f(...args), a, b)`
+ *
+ * **Note:**
+ * This is a temporary solution to replace yet-to-be-implemented pipeline operator.
+ * This function will be deprecated when the proposal get to stage 3
+ * and removed when it is implemented
+ *
+ * Read more about pipeline operator in
+ * [the tc39 proposal](https://github.com/tc39/proposal-pipeline-operator)
+ *
  * @param fn First function in the pipeline
  * @returns First node of the pipeline
- *
- * @usage
- *   `pipe(f).to(g).to(h).get(...args)` is equivalent to `f(...args) |> g |> h`,
- *   which in turn is equivalent to `h(g(f(...args)))`
- *
- *   You can also pass extra arguments to function that takes more than takes more than one arguments,
- *   for example: `pipe(f).to(g, a, b).get` is equivalent to `(...args) => g(f(...args), a, b)`
- *
- * @note
- *   This is a temporary solution to replace yet-to-be-implemented pipeline operator.
- *   This function will be deprecated when the proposal get to stage 3
- *   and removed when it is implemented
- *
- *   Read more about pipeline operator in
- *   [the tc39 proposal](https://github.com/tc39/proposal-pipeline-operator)
- *
- * @example
- *   const sum = (...args: number[]) => args.reduce((a, b) => a + b, 0)
- *   const increase = (x: number) => x + 1
- *   const double = (x: number) => x * 2
- *   const square = (x: number) => x * x
- *   const pipeline = pipe(sum) // x0 = sum(...args)
- *     .to(increase) // x1 = increase(x0)
- *     .to(double) // x2 = double(x1)
- *     .to(square) // x3 = square(x2)
- *     .to(String) // x4 = String(x3)
- *     .get // pipeline(...args) = x4 = String(square(double(increase(sum(...args)))))
- *   expect(pipeline(2, 3, -1)).toBe(String(square(double(increase(sum(2, 3, -1))))))
- *
- * @example
- *   const sum = (...args: number[]) => args.reduce((a, b) => a + b)
- *   const product = (...args: number[]) => args.reduce((a, b) => a * b)
- *   const pipeline = pipe(sum) // x0 = sum(...args)
- *     .to(product, 2) // x1 = product(x0, 2)
- *     .to(String) // x2 = String(x1)
- *     .get // pipeline(...args) = x2 = String(product(sum(2, -3, 4), 2))
- *   expect(pipeline(2, -3, 4)).toBe(String(product(sum(2, -3, 4), 2)))
  */
 export const pipe =
   <Args extends any[], Return>
--- packages/tap/index.ts
+++ packages/tap/index.ts
@@ -8,20 +8,24 @@ export interface SideEffect<Value> {
 
 /**
  * Insert a side-effect into function pipeline
+ *
+ * **Example:**
+ *
+ * ```typescript
+ * const result = pass(value)
+ *   .to(tap, value => console.log('begin', value))
+ *   .to(inc)
+ *   .to(tap, value => console.log('after inc', value))
+ *   .to(double)
+ *   .to(tap, value => console.log('after double', value))
+ *   .to(square)
+ *   .to(tap, value => console.log('after square', value))
+ *   .get()
+ * ```
+ *
  * @param value Value from pipeline
  * @param fns Side effect functions
  * @returns `value`
- *
- * @example
- *   const result = pass(value)
- *     .to(tap, value => console.log('begin', value))
- *     .to(inc)
- *     .to(tap, value => console.log('after inc', value))
- *     .to(double)
- *     .to(tap, value => console.log('after double', value))
- *     .to(square)
- *     .to(tap, value => console.log('after square', value))
- *     .get()
  */
 export function tap<Value> (value: Value, ...fns: SideEffect<Value>[]): Value {
   fns.forEach(fn => fn(value))
@@ -41,19 +45,23 @@ export interface Tapper<Value> {
  * Insert a side-effect into function pipeline.
  * This is used in situation where `tap` is not suited,
  * such as `map`
+ *
+ * **Example:**
+ *
+ * ```typescript
+ * const myResult = pass(result)
+ *   .to(tap, result => console.log('result before onOk/onErr', result))
+ *   .to(map, tapper(value => console.log('value before onOk', value)))
+ *   .to(mapErr, tapper(error => console.log('error before onErr', error)))
+ *   .to(map, onOk)
+ *   .to(map, tapper(value => console.log('value after onOk', value)))
+ *   .to(mapErr, onErr)
+ *   .to(mapErr, tapper(error => console.log('error after onErr', error)))
+ *   .get()
+ * ```
+ *
  * @param fns Side effect functions
  * @returns A side-effect function that can be inserted into a pipeline
- *
- * @example
- *   const myResult = pass(result)
- *     .to(tap, result => console.log('result before onOk/onErr', result))
- *     .to(map, tapper(value => console.log('value before onOk', value)))
- *     .to(mapErr, tapper(error => console.log('error before onErr', error)))
- *     .to(map, onOk)
- *     .to(map, tapper(value => console.log('value after onOk', value)))
- *     .to(mapErr, onErr)
- *     .to(mapErr, tapper(error => console.log('error after onErr', error)))
- *     .get()
  */
 export function tapper<Value> (...fns: SideEffect<Value>[]): Tapper<Value> {
   return value => tap(value, ...fns)
