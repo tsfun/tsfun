6810d55c65db7c6e900ca1179e20655e12ca3bff
khai96_ <hvksmr1996@gmail.com>
2019-10-06 13:09:49 +0700

    Add assertion that option of undefined | null returns None

--- test/option/typecheck/option.ts
+++ test/option/typecheck/option.ts
@@ -1,12 +1,15 @@
 import assert from 'static-type-assert'
 import { Option, Some, None, option } from '@tsfun/option'
 
+const ANY: any = null
+
 assert<Some<'x'>>(option('x' as 'x'))
 assert<Some<0>>(option(0 as 0))
 assert<Some<false>>(option(false as false))
 
 assert<None>(option(undefined))
 assert<None>(option(null))
+assert<None>(option(ANY as undefined | null))
 
 assert<Option<any>>(option(0 as any))
 assert<Option<0>>(option(0 as 0 | undefined))
