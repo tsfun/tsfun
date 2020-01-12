45c9eeefdd6123d2ac2d67c551cc151f169176e2
khai96_ <hvksmr1996@gmail.com>
2020-01-12 10:28:49 +0700

    Stricter

--- packages/object/property-path.ts
+++ packages/object/property-path.ts
@@ -1,31 +1,33 @@
 type Key = string | symbol | number
 
+const isObject = (object: any): object is object => typeof object === 'object' && object
+
 export function getPropertyPath (object: any, path: readonly Key[]): any {
   if (!path.length) return object
-  if (object === undefined || object === null) return undefined
+  if (!isObject(object)) return undefined
 
   const [key, ...rest] = path
 
   return key in object
-    ? getPropertyPath(object[key], rest)
+    ? getPropertyPath((object as any)[key], rest)
     : undefined
 }
 
 export function setPropertyPath (object: any, path: readonly Key[], value: any): any {
   if (!path.length) return value
-  if (object === undefined || object === null) return setPropertyPath({}, path, value)
+  if (!isObject(object)) return setPropertyPath({}, path, value)
 
   const [key, ...nextPath] = path
-  const { [key]: nextTarget, ...cloned } = object
+  const { [key]: nextTarget, ...cloned } = object as any
   cloned[key] = setPropertyPath(nextTarget, nextPath, value)
   return cloned
 }
 
 export function deletePropertyPath (object: any, path: readonly Key[]): any {
-  if (!path.length || object === undefined || object === null) return undefined
+  if (!path.length || !isObject(object)) return undefined
 
   const [key, ...nextPath] = path
-  const { [key]: nextTarget, ...cloned } = object
+  const { [key]: nextTarget, ...cloned } = object as any
 
   if (!nextPath.length) return cloned
 
