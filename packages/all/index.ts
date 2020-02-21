719bf9274c6edf9bd15cb371f15be6f84ab2aece
khai96_ <hvksmr1996@gmail.com>
2020-02-21 12:29:56 +0700

    Use 'export * as' syntax

--- packages/all/index.ts
+++ packages/all/index.ts
@@ -1,23 +1,8 @@
-import * as Function from '@tsfun/function'
-export { Function }
-
-import * as Pipe from '@tsfun/pipe'
-export { Pipe }
-
-import * as Option from '@tsfun/option'
-export { Option }
-
-import * as Result from '@tsfun/result'
-export { Result }
-
-import * as Object from '@tsfun/object'
-export { Object }
-
-import * as Array from '@tsfun/array'
-export { Array }
-
-import * as Tap from '@tsfun/tap'
-export { Tap }
-
-import * as Misc from '@tsfun/misc'
-export { Misc }
+export * as Function from '@tsfun/function'
+export * as Pipe from '@tsfun/pipe'
+export * as Option from '@tsfun/option'
+export * as Result from '@tsfun/result'
+export * as Object from '@tsfun/object'
+export * as Array from '@tsfun/array'
+export * as Tap from '@tsfun/tap'
+export * as Misc from '@tsfun/misc'
