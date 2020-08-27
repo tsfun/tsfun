87621d03b87075b6866782ade62f22108734ba61
khai96_ <hvksmr1996@gmail.com>
2020-08-27 09:07:59 +0700

    Run sane-fmt

--- packages/array/last.ts
+++ packages/array/last.ts
@@ -3,8 +3,7 @@
  * @param array Array to get last item from
  * @returns Last item of `array`
  */
-export const last = <Body extends any[], Last>(array: readonly [...Body, Last]): Last =>
-  array[array.length - 1] as any
+export const last = <Body extends any[], Last>(array: readonly [...Body, Last]): Last => array[array.length - 1] as any
 
 /**
  * Get last item of an array
