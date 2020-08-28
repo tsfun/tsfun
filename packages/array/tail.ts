84aa7a84e75764bfa79d2b3d2f35710c577edbd8
khai96_ <hvksmr1996@gmail.com>
2020-08-28 11:05:02 +0700

    Split 'tail' into 'tail' and 'tailStrict'

--- packages/array/tail.ts
+++ packages/array/tail.ts
@@ -1,22 +1,13 @@
-interface Tail {
-  /**
-   * Get all but first items of an array
-   * @param array Input array
-   * @returns All but first items of `array`
-   */
-  <Tail extends any[]>(array: readonly [any, ...Tail]): Tail
-
-  /**
-   * Get all but first items of an array
-   * @param array Input array
-   * @returns All but first items of `array`
-   */
-  <Item>(array: readonly Item[]): Item[]
-}
+/**
+ * Get all but first items of an array
+ * @param array Input array
+ * @returns All but first items of `array`
+ */
+export const tail = <Item>(array: readonly Item[]): Item[] => array.slice(1)
 
 /**
  * Get all but first items of an array
  * @param array Input array
  * @returns All but first items of `array`
  */
-export const tail: Tail = ((array: any[]) => array.slice(1)) as any
+export const tailStrict: (<Tail extends any[]>(array: readonly [any, ...Tail]) => Tail) = tail as any
